310bb2ea73e5edd9b4cb13799be67bd174b3a9b7
itsalessandroricci <alessandro.ricci.rcc@gmail.com>
2024-02-16 12:05:50 +0100

    Up test

--- Script/Script/ContentView.swift
+++ Script/Script/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "sun")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
