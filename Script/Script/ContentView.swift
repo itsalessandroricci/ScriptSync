49af464e99802158c3f386ceb810feeabd341991
itsalessandroricci <alessandro.ricci.rcc@gmail.com>
2024-02-16 12:12:13 +0100

    Test

--- Script/Script/ContentView.swift
+++ Script/Script/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
-            Text("prova")
+            Text("pro")
         }
         .padding()
     }
