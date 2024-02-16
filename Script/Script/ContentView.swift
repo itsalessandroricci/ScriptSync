773476c7dd39a2fbc024bb093e8cbe5592711508
Luigi Emanuele Zippo <luigi.academy@icloud.com>
2024-02-16 12:10:38 +0100

    Test

--- Script/Script/ContentView.swift
+++ Script/Script/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("prova")
+            Text("Pall")
         }
         .padding()
     }
