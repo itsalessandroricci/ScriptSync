2e8e0eaaedc27e145da9bde3ea7d68dd2601ee5f
itsalessandroricci <alessandro.ricci.rcc@gmail.com>
2024-02-16 11:55:48 +0100

    First push

---
+++ Script/Script/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Script
+//
+//  Created by Alessandro Ricci on 16/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Script/Script/ScriptApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScriptApp.swift
+//  Script
+//
+//  Created by Alessandro Ricci on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ScriptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
